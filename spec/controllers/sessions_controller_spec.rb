d6fcb125b7c69f02b3686231aa0a66f60d229ad2
Rick Mark <rickmark@outlook.com>
2014-09-15 16:21:26 -0700

    CSRF fix

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe SessionsController, :type => :controller do
       params = { state: 'mobile_connected',
                  facebook_auth_token: 'CAAKI8px4npABAKA93g6tlawX77ZC8pIKcU6163SG7ZCV99ffMQ8GNBO8ZAUhQ9sIe6B0ImVnAcV8qgPXuJIJGtsMphNHZAjMTDL4n5op0L3iwETqrBemAZAV0vF9g5DQKZA7Lvpei9ONIELgV1F7jmWt2CRhAoc81qpiZBFhQwhxrJ5ZAjyPY8TFZAMMUINUkcMiz15IGJQUmW94XyQdSMSjwZCG1Lpa8AOZAsYGxZCH2e9jVQZDZD'}
 
-      post :mobile_create, params
+      post :token, params
 
       response_object = JSON.parse(response.body)
 
