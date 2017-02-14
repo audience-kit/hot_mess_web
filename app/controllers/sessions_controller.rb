5fcc55226800b3da54717c786c26c519251acd0d
Rick Mark <rickmark@outlook.com>
2017-02-13 23:50:56 -0500

    Fix for parsing error with signed request

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,18 @@ class SessionsController < ApplicationController
     logger.debug "\tFacebook login with status #{facebook_session_params[:status]}"
 
     if facebook_session_params[:status] == 'connected'
-      auth_response = Facebook.signed_message(facebook_session_params[:authResponse][:signedRequest])
-      logger.debug "\tReceived Facebook signed authentication message #{auth_response.inspect}"
+      #auth_response = Facebook.signed_message(facebook_session_params[:authResponse][:signedRequest])
+      #logger.debug "\tReceived Facebook signed authentication message #{auth_response.inspect}"
 
-      access_token_info = Facebook.oauth.get_access_token_info(auth_response['code'])
-      logger.debug "\tGot access token info from Facebook #{access_token_info.inspect}"
+      #access_token_info = Facebook.oauth.get_access_token_info(auth_response['code'])
+      #logger.debug "\tGot access token info from Facebook #{access_token_info.inspect}"
 
-      session[:facebook_access_token] = access_token_info['access_token']
-      session[:facebook_access_expires] = access_token_info['expires'].to_i
+      session[:facebook_access_token] = params[:authResponse][:accessToken]
+      session[:facebook_access_expires] = params[:authResponse][:expiresIn].to_i
 
       token_connection = Faraday.new url: Rails.application.secrets.api_location
 
-      response = token_connection.post '/token', facebook_token: access_token_info['access_token'], device: { type: 'web', identifier: request.remote_ip }
+      response = token_connection.post '/token', facebook_token: session[:facebook_access_token], device: { type: 'web', identifier: request.remote_ip }
 
       if response.status == 200
         session[:session_valid] = true
