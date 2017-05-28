16d7487eb204d2d1330de8228f4cb4311f44158a
Rick Mark <rickmark@outlook.com>
2017-05-28 13:20:26 -0700

    Alexa grant flow

---
+++ app/controllers/alexa_controller.rb
@@ -0,0 +1,5 @@
+class AlexaController < ApplicationController
+  VALID_REDIRECTS = [ 'https://layla.amazon.com/api/skill/link/MXJZFEH4VC4HU', 'https://pitangui.amazon.com/api/skill/link/MXJZFEH4VC4HU' ]
+
+  CLIENT_SECRET = 'Z14rpM0Pqew/t44ysFOSD7XKqSnmCUSrie6Zwlt3NSc='
+end
